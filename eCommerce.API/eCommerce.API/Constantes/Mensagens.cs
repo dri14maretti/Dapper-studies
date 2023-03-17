d21812b777c81052af84522e10413758c9ecdc14
Adriano <dri.cmaretti@gmail.com>
2023-03-17 19:09:32 -0300

    alterações

---
+++ eCommerce.API/eCommerce.API/Constantes/Mensagens.cs
@@ -0,0 +1,7 @@
+﻿namespace eCommerce.API.Constantes
+{
+    public class Mensagens
+    {
+        public const string InjetarPropriedades = "Erro ao injetar as propriedades na controller";
+    }
+}
--- eCommerce.API/eCommerce.API/Models/User.cs
+++ eCommerce.API/eCommerce.API/Models/User.cs
@@ -1,9 +1,20 @@
-﻿namespace eCommerce.API.Models
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace eCommerce.API.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Sex { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+        public string MotherNome{ get; set; }
+        public string RegistrationSituation { get; set; }
+        public DateTimeOffset RegistrationDate { get; set; }
+        public Contact Contact { get; set; }
+        public ICollection<Address> DeliveryAddresses { get; set; }
+        public ICollection<Departament> Departaments { get; set; }
     }
 }
